83ce34b30107dd48de614d26cce59633852c083d
merk Choi <team.gghf@gmail.com>
2019-06-08 14:46:16 +0900

    no message

---
+++ Assets/Scripts/Enemy1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+
+    public GameObject Cathle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cathle = GameManager.FindObjectOfType(Cathle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 followVec3;
+
+        followVec3 = (Cathle.transform.position- this.gameObject.transform.position).normalized;
+        this.transform.Translate(followVec3 * Time.deltaTime);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,23 +10,41 @@ public class GameManager : MonoBehaviour
     public GameObject Enemy1;
     public GameObject Enemy2;
     public GameObject Enemy3;
-    
+    int DirE;
+    public GameObject Pos1;
+    public GameObject Pos2;
+    public GameObject Pos3;
+public GameObject Pos4;
+    public enum Driection {NORTH, SOUTH, WEST, EAST}
     // Start is called before the first frame update
+    Driection dir;
+
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
         deltatime += Time.deltaTime;
-
         if(deltatime > 2)
-        {
-             Instantiate(Enemy1, this.transform.position + Vector3.forward*3, transform.rotation);
-    }
+        {              
+         DirE = UnityEngine.Random.RandomRange(1,4);
 
+            if(DirE == 1)
+             Instantiate(Enemy1, Pos1.transform.position, transform.rotation);
+            if(DirE == 2)
+            Instantiate(Enemy1, Pos2.transform.position, transform.rotation);
+            if(DirE == 3)
+            Instantiate(Enemy1, Pos3.transform.position, transform.rotation);
+                  if(DirE == 4)
+            Instantiate(Enemy1, Pos4.transform.position, transform.rotation);
+            
+             deltatime = 0f;
         }
+
+    }
        
 }
